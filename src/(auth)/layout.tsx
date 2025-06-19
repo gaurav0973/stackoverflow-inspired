0ca7b2a6d9dc2df76fe448109765b6c45defac81
gaurav0973 <codejourneypanda@gmail.com>
2025-06-19 09:46:52 +0530

    register and login page is being completed

--- src/(auth)/layout.tsx
+++ src/(auth)/layout.tsx
@@ -1,14 +1,15 @@
 "use client";
+
 import { useAuthStore } from "@/store/Auth"
 import { useRouter } from "next/navigation";
-import React, { useEffect } from "react";
+import React from "react";
 
 
 const Layout = ({children}: {children: React.ReactNode}) => {
   const {session} = useAuthStore();
   const router = useRouter()
 
-  useEffect(() => {
+  React.useEffect(() => {
     if (session) {
       router.push("/")
     }
